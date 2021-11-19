4e76d22f005f1936e8fda6f772799bc5814634b1
Theoshan <theoshan@gmail.com>
2021-11-19 19:43:51 +0200

    Added Data Studio Report

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { WebView } from 'react-native-webview';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const initialHTMLContent = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="author" content="">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src='https://unpkg.com/rahulrsingh09-stenciltest2@0.0.3/dist/test/test.js'></script>
+</head>
+<body>
+<h1>Hello</h1>
+<iframe width="600" height="450" src="https://datastudio.google.com/embed/reporting/c1e3b11a-5682-4558-ba23-07a3109dbf34/page/kmofC" frameborder="0" style="border:0" allowfullscreen></iframe>
+</body>
+</html>`;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    
+    <View
+    style={{
+      flex: 1,
+    }}>
+    <WebView
+      originWhitelist={['*']}
+      javaScriptEnabled={true}
+      domStorageEnabled={true}
+      source={{
+        html: initialHTMLContent,
+        
+      }}
+    />
+  </View>
+   
+  );  
 }
 
 const styles = StyleSheet.create({
